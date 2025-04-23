b0433d8c37b6bf13d9cb2663f4957322dcad106c
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2025-04-23 22:03:39 +0200

    std::thead::id information added

--- Chapter-2. Managing Threads/2.1.6_ContainarisedThreads.cpp
+++ Chapter-2. Managing Threads/2.1.6_ContainarisedThreads.cpp
@@ -5,17 +5,26 @@
 
 // Move Semantics allow us to pass threads to containers
 
-void function(int id) {
-  for(int i = 0; i < 16; i++) {
-    std::cout << "thread id = " << id << " iteration = " << i << std::endl;
+void function() {
+  for(int i = 0; i < 2; i++) {
+    // std::cout on the thread ID have no semantic meaning, but it is guaranteed that the if will be comparable
+    // with other ids,and the < and > operations will give the expected results
+    std::cout << "thread id = " << std::this_thread::get_id() << " iteration = " << i << std::endl;
   }
 }
 
+//std::thread::id is a unique identifier for the thread that works in most circunstances
+// It provides a set of comparison
+// It also provide a std::hash<std::thread::id> if you need an associative container
 
 // The code is problematic since the iostream suffer a heavy race condition here. But it is just to show
 int main() {
   std::vector<std::thread> threads;
   for (int i = 0; i < 30;i++) {
-    threads.emplace_back(function,i); // use emplace for the move semantics
+    threads.emplace_back(function); // use emplace for the move semantics
+  }
+
+  for (auto &t : threads) {
+    t.join();
   }
 }
